02410213ac4f2682cbed8376a72798af5776daaf
Peter M. Elias <petermelias@gmail.com>
2013-10-15 17:40:29 -0400

    adding initial fxns and utils

---
+++ mrpython/fxn.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import inspect
+import socket
+import struct
+
+def autolog(message=None, level=logging.DEBUG):
+	func = inspect.currentframe().f_back.f_code
+	frame = inspect.stack()[1]
+	values = inspect.formatargvalues(*inspect.getargvalues(frame[0]))
+	module = inspect.getmodule(frame[0])
+	logger = logging.getLogger(module.__name__)
+	logger.log(level, '%s [%s] : %s' % (func.co_name, values, message))
+
+def enum(**enums):
+	return type('Enum', (), dict(enums.items() + {'items': enums}.items()))
+
+def ip2long(ip):
+	packed = socket.inet_aton(ip)
+	return struct.unpack('!L', packed)[0]
+
+def long2ip(lg):
+	return socket.inet_ntoa(struct.pack('!L', lg))
---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+	name="mrpython",
+	version="0.0.1",
+	url='http://github.com/petermelias/mrpython',
+	license='MIT',
+	author='Peter M. Elias',
+	author_email='petermelias@gmail.com',
+	description='Collection of things for Python that dont warrant their own libraries.',
+	packages=find_packages(),
+	install_requires=[],
+	extras_require={
+		'test': ['nose', 'coveralls']
+	},
+	classifiers=[
+		'Environment :: Web Environment',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+		'Topic :: Software Development :: Libraries :: Python Modules'
+	]
+)
