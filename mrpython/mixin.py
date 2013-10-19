90f36711cef75386ebcca3c15342c34d561e4d37
Peter M. Elias <petermelias@gmail.com>
2013-10-18 22:04:07 -0400

    fixed a very subtle bug in the recursive behavior of to_dict. was not playing nice with certain path traversal scenarios. all set now :)

--- mrpython/mixin.py
+++ mrpython/mixin.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
+
+from pprint import pprint
+
 from datetime import datetime
+
+def _step():
+	raw_input('Step >')
 
 class DictInterface(object):
 
@@ -11,25 +17,31 @@ class DictInterface(object):
 
 	def to_dict(self, only=[], date_format='%Y-%m-%d %H:%M:%S'):
 
-		def _obj_to_dict(o, limit=[]):
+		def _obj_to_dict(o, limit=[], terminate=False):
 			names = o._dict_attrs or filter(lambda x: not x.startswith('_'), vars(o))
 			if limit:
 				names = list(set(limit).intersection(set(names)))
+
+			if terminate:
+				return {k: getattr(o, k) for k in names if not isinstance(getattr(o,k), DictInterface)}
 			return {k: getattr(o, k) for k in names}
 
-		def _walk(o, parents=[], limit=[]):
+		def _walk(o, path=[], prev=None, limit=[]):
 			c = {}
 			d = _obj_to_dict(o, limit)
+			
+			if prev:
+				path.append(prev)
+			
 			for attr, value in d.iteritems():
 				if isinstance(value, DictInterface):
-					if value is o:
+					if path and (value is path[0]):
 						continue
-					if value in parents:
+					if value is prev:
 						continue
-					parents.append(o)
-					c[attr] = _walk(value, parents)
+					c[attr] = _walk(value, path, o)
 				elif hasattr(value, '__iter__'):
-					c[attr] = [_walk(v, parents) for v in value]
+					c[attr] = [_walk(v, path, o) for v in value]
 				elif isinstance(value, datetime):
 					c[attr] = value.strftime(date_format)
 				else:
